571a48b8653cefaaec48bc9c2e5f4db237675dd0
David Rey <david.rey.acosta@gmail.com>
2012-10-22 18:24:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
     <body>
         <div id="main">
             <header>
-<span>Automatic pull</span>
                 <h1>Responsive CSS3 Slider</h1>
                 <p>A pure responsive CSS3 Slider without Javascript</p>
                 <p><em><strong>by <a href="http://dreyacosta.com" target=_blank>David Rey</a></strong></em></p>
